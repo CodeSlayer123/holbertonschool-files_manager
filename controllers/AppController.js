95087394aca5619fcb4effde6d75953c7c5d3bff
Code Slayer <3613@holbertonschool.com>
2022-11-23 08:57:47 -0600

    task 2 test

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,13 +2,12 @@ import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
 class AppController{
-    static getStatus(request, response){
-        if (dbClient.isAlive() && redisClient.isAlive()){
-            return response.status(200).json({ redis: true, db: true})
-
+    static getStatus(req, res) {
+        if (Redis.isAlive() && DB.isAlive()) {
+          return res.status(200).json({ redis: true, db: true });
         }
-        return response.status(400).send('Redis and MongoDB not connected')
-    }
+        return res.status(400).send('Redis and MongoDB not connected');
+      }
     static async getStats(request, response){
         const users = dbClient.nbUsers()
         const files = dbClient.nbFiles()
